f0590e6f241e0d5f63a90268c327f813677aa12e
guntz <rricardoguntzell@gmail.com>
2025-05-22 16:01:30 -0300

    inclusao do git modules

---
+++ src/test/java/br/com/guntz/sensors/temperature/monitoring/TemperatureMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guntz.sensors.temperature.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
